c0c453f7faa96970f6fec6d1d7c189db30a2b969
greysky <antonysnork@gmail.com>
2022-09-03 18:17:28 +0300

    main update

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,36 +1,5 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+
 import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
-
-    @Test
-    public void shouldGetAllFilms() {
-        CashbackHackService service = new CashbackHackService();
-
-int expected = 800;
-int actual = service.remain(200);
-        Assert.assertEquals(actual, expected);
-
-    }
-    @Test
-    public void shouldGetAllFilmsa() {
-        CashbackHackService service = new CashbackHackService();
-
-        int expected = 1;
-        int actual = service.remain(999);
-        Assert.assertEquals(actual, expected);
-
-    }
-    @Test
-    public void shouldGetAllFilmsaa() {
-        CashbackHackService service = new CashbackHackService();
-
-        int expected = 0;
-        int actual = service.remain(1000);
-        Assert.assertEquals(actual, expected);
-
-    }
-
 }
