827c04a769693d9c437ee8cab1acd6956c17fc38
greysky <antonysnork@gmail.com>
2022-09-03 18:14:03 +0300

    junit tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
 package ru.netology.service;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldSetHighBound() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldSetLowBound() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldSetEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+
+    }
+
 }
+
