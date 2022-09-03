3230313e14143bc556f93179025a2b80b05cfafd
greysky <antonysnork@gmail.com>
2022-09-03 19:24:17 +0300

    add junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 public class CashbackHackServiceTest {
@@ -11,27 +12,59 @@ public class CashbackHackServiceTest {
 
         int expected = 999;
         int actual = service.remain(1001);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
 
     }
+
     @Test
     public void shouldSetLowBound() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 1;
         int actual = service.remain(999);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
 
     }
+
     @Test
     public void shouldSetEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 0;
         int actual = service.remain(1000);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
 
     }
 
+    @Test
+    public void shouldSetHighBoundJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldSetLowBoundJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldSetEqualBoundaryJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected,actual);
+
+    }
 }
 
